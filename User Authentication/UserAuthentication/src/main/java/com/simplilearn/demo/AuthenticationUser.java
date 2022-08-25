038d1c016cd386d47ba1bf33a070922bc72eb190
SweathaRJ <109415590+SweathaRJ@users.noreply.github.com>
2022-08-25 19:19:35 +0530

    User Authentication

---
+++ User Authentication/UserAuthentication/src/main/java/com/simplilearn/demo/AuthenticationUser.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class AuthenticationUser {
+	public String username()
+	{
+		String email = "swea@gmail.com";
+		return email;
+	}
+	
+	public String paswd()
+	{
+		String password = "swea";
+		return password;
+	}
+
+}
